4b8564b67330c25ebb193099f77bb17bdef9fee6
patpat241 <patwnnu@gmail.com>
2025-05-28 10:53:52 +0800

    Updated Ui for buildings modals

--- src/pages/resort_admin/modals/buildings/DeleteBuildingModal.jsx
+++ src/pages/resort_admin/modals/buildings/DeleteBuildingModal.jsx
@@ -1,9 +1,17 @@
-import React from 'react'
+import React from "react";
 
-const DeleteBuildingModal = () => {
+const DeleteBuildingModal = ({ building }) => {
   return (
-    <div>DeleteBuildingModal</div>
-  )
-}
+    <div className="space-y-4 text-sm">
+      <p>
+        Are you sure you want to delete the building{" "}
+        <strong className="text-red-600">{building?.name}</strong>?
+      </p>
+      <p className="text-gray-600">
+        This action is irreversible and will remove all associated rooms and floors.
+      </p>
+    </div>
+  );
+};
 
-export default DeleteBuildingModal
+export default DeleteBuildingModal;
